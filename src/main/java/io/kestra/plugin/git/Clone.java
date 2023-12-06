f7fa54058f5acc1559df448628fcc3fbb95aed75
brian.mulier <bmmulier@hotmail.fr>
2023-12-06 18:53:18 +0100

    fix(tasks): Clone task properly outputs clone destination directory instead of .git directory + private repositories testing

--- src/main/java/io/kestra/plugin/git/Clone.java
+++ src/main/java/io/kestra/plugin/git/Clone.java
@@ -4,26 +4,19 @@ import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.tasks.RunnableTask;
-import io.kestra.core.models.tasks.Task;
 import io.kestra.core.runners.RunContext;
-import io.kestra.plugin.git.services.SshTransportConfigCallback;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.slf4j.Logger;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.Set;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.nio.file.Path;
 
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
 @ToString
 @EqualsAndHashCode
 @Getter
@@ -43,7 +36,7 @@ import javax.validation.constraints.NotNull;
         @Example(
             title = "Clone a private repository from an HTTP server such as a private GitHub repository using a [personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)",
             code = {
-                "url: https://github.com/anna-geller/kestra-flows",
+                "url: https://github.com/kestra-io/examples",
                 "branch: main",
                 "username: anna-geller",
                 "password: your_personal_access_token"
@@ -54,7 +47,7 @@ import javax.validation.constraints.NotNull;
             code = {
                 "url: git@github.com:kestra-io/kestra.git",
                 "directory: kestra",
-                "privateKey: <keyfile>",
+                "privateKey: <keyfile_content>",
                 "passphrase: <passphrase>"
             }
         ),
@@ -71,7 +64,7 @@ import javax.validation.constraints.NotNull;
                 "    tasks:",
                 "      - id: cloneRepository",
                 "        type: io.kestra.plugin.git.Clone",
-                "        url: https://github.com/anna-geller/kestra-flows",
+                "        url: https://github.com/kestra-io/examples",
                 "        branch: main",
 
                 "      - id: pythonETL",
@@ -79,19 +72,12 @@ import javax.validation.constraints.NotNull;
                 "        beforeCommands:",
                 "          - pip install requests pandas > /dev/null",
                 "        commands:",
-                "          - ./bin/python flows/etl_script.py",
+                "          - ./bin/python scripts/etl_script.py",
             }
         )
     }
 )
-public class Clone extends Task implements RunnableTask<Clone.Output> {
-    @Schema(
-        title = "The URI to clone from"
-    )
-    @PluginProperty(dynamic = true)
-    @NotNull
-    private String url;
-
+public class Clone extends AbstractGitTask implements RunnableTask<Clone.Output> {
     @Schema(
         title = "The optional directory associated with the clone operation.",
         description = "If the directory isn't set, the current directory will be used."
@@ -99,16 +85,12 @@ public class Clone extends Task implements RunnableTask<Clone.Output> {
     @PluginProperty(dynamic = true)
     private String directory;
 
-    @Schema(
-        title = "The initial Git branch."
-    )
-    @PluginProperty(dynamic = true)
     private String branch;
 
     @Schema(
         title = "Creates a shallow clone with a history truncated to the specified number of commits."
     )
-    @PluginProperty(dynamic = false)
+    @PluginProperty
     @Builder.Default
     @Min(1)
     private Integer depth = 1;
@@ -116,32 +98,8 @@ public class Clone extends Task implements RunnableTask<Clone.Output> {
     @Schema(
         title = "Whether to clone submodules."
     )
-    @PluginProperty(dynamic = false)
+    @PluginProperty
     private Boolean cloneSubmodules;
-
-    @Schema(
-        title = "The username or organization."
-    )
-    @PluginProperty(dynamic = true)
-    private String username;
-
-    @Schema(
-        title = "The password or personal access token."
-    )
-    @PluginProperty(dynamic = true)
-    private String password;
-
-    @Schema(
-        title = "The private keyfile used to connect."
-    )
-    @PluginProperty(dynamic = true)
-    protected String privateKey;
-
-    @Schema(
-        title = "The passphrase for the `privateKey`."
-    )
-    @PluginProperty(dynamic = true)
-    protected String passphrase;
 
     @Override
     public Clone.Output run(RunContext runContext) throws Exception {
@@ -175,35 +133,22 @@ public class Clone extends Task implements RunnableTask<Clone.Output> {
             cloneCommand.setCloneSubmodules(this.cloneSubmodules);
         }
 
-        if (this.username != null && this.password != null) {
-            cloneCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(
-                runContext.render(this.username),
-                runContext.render(this.password)
-            ));
-        }
-
-        if (this.privateKey != null) {
-            Path privateKey = runContext.tempFile(
-                runContext.render(this.privateKey).getBytes(StandardCharsets.UTF_8),
-                ""
-            );
-
-            Files.setPosixFilePermissions(privateKey, Set.of(PosixFilePermission.OWNER_READ));
-
-            cloneCommand.setTransportConfigCallback(new SshTransportConfigCallback(
-                privateKey.toFile(),
-                runContext.render(this.passphrase)
-            ));
-        }
+        cloneCommand = authentified(cloneCommand, runContext);
 
         logger.info("Start cloning from '{}'", url);
 
         try (Git call = cloneCommand.call()) {
             return Output.builder()
-                .directory(call.getRepository().getDirectory().getAbsolutePath())
+                .directory(call.getRepository().getDirectory().getParent())
                 .build();
         }
-}
+    }
+
+    @Override
+    @NotNull
+    public String getUrl() {
+        return super.getUrl();
+    }
 
     @Builder
     @Getter
