60dae8e7710724bd3f8b63d23aac1e3ba99ec67c
ben8t <pimpaudben@gmail.com>
2025-12-30 15:37:45 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/git/package-info.java
+++ src/main/java/io/kestra/plugin/git/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Git.\n" +
         "Git is a distributed version control system that tracks changes in any set of computer files, usually used for coordinating work among programmers collaboratively developing source code during software development. Its goals include speed, data integrity, and support for distributed, non-linear workflows.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE,
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.git;
 
